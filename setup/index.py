14b6bf26b6c8389b2299e0e76696ac6ba4ca5f05
Katsutoshi Seki <sekika@users.noreply.github.com>
2015-12-15 04:18:03 +0900

    html

--- setup/index.py
+++ setup/index.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # http://sekika.github.io/search/ の index.js を生成するスクリプト
+# 生成されたファイル: http://sekika.github.io/js/index.js
+
+# pip3 install markdown
 
 # ファイルの場所指定
 post_dir = '../_posts/' # _posts/ ディレクトリ
@@ -31,6 +34,7 @@ def strip_tags(html):
 # 処理の開始
 import os
 import time
+import markdown
 f = open(index, 'w')
 f.write('var start = new Date().getTime();\n')
 f.write('var data = [')
@@ -62,7 +66,7 @@ for i in os.listdir(post_dir):
     f.write(i.replace('-','/',3).replace('.md','').replace('.markdown',''))
     f.write('/",title:"'+title+'",mtime:'+str(update)+',ctime:'+str(date)+',body:"'+title+' ')
     for line in data:
-        f.write(strip_tags(line).rstrip("\n").replace('"',' ').replace('`','').replace('#','').replace('~~~',' '))
+        f.write(strip_tags(markdown.markdown(line)).replace("\n",'').replace('"',' ').replace('~~~',''))
     data.close()
     f.write('"}')
 f.write('];\n')
