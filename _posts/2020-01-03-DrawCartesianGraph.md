5c73cf1e48099f63aa2eefb54204fe7553a1b6a8
Katsutoshi Seki <sekika@users.noreply.github.com>
2020-01-05 04:17:59 +0900

    パラメータと説明の整理

--- _posts/2020-01-03-DrawCartesianGraph.md
+++ _posts/2020-01-03-DrawCartesianGraph.md
@@ -2,7 +2,7 @@
 layout: post
 title: 関数のグラフを描画するプログラム
 date: 2020-01-03 12:33:17 +0000
-update: 2020-01-04 18:34:43 +0000
+update: 2020-01-04 22:04:06 +0000
 tags:
 - jekyll
 - javascript
@@ -38,8 +38,8 @@ ctx.unitY = -50;
 ctx.strokeStyle = "black";
 ctx.fillStyle = "black";
 drawAxis(ctx);
-drawScaleX(ctx, 1);
-drawScaleY(ctx, 1);
+drawScaleX(ctx);
+drawScaleY(ctx);
 ctx.strokeStyle = "blue";
 draw(ctx, exp);
 ctx.fillStyle = "blue";
@@ -60,17 +60,23 @@ function exp(x) {
 </script>
 <!-- -------------------------------------------------------------------------------------------- -->
 
-このページの[ソース](https://raw.githubusercontent.com/sekika/sekika.github.io/master/_posts/2020-01-03-DrawCartesianGraph.md)を表示して、`<canvas id="canvas" width="600" height="600">`から`</script>`までをコピーしてHTML文書内に貼り付け、適宜書き換えることで指定したグラフを描画することができる。
+## 使い方
 
+以下のようにして、指定した関数のグラフを描画することができる。
+
+- このページの[ソース](https://raw.githubusercontent.com/sekika/sekika.github.io/master/_posts/2020-01-03-DrawCartesianGraph.md)を表示して、`<canvas id="canvas" width="600" height="600">`から`</script>`までをコピーしてHTML文書内に貼り付け、適宜書き換える。
 - 描画エリア canvas の大きさは `<canvas id="canvas" width="600" height="600">` の width と height で指定する。
-- `<script src="https://sekika.github.io/js/graph.js"></script>` では、[graph.js](https://sekika.github.io/js/graph.js) のURLを指定する。このサーバー上の graph.js が書き換えられる可能性があるため、コピーして使うことを推奨する。[graph.js をGitHub で表示](https://github.com/sekika/sekika.github.io/blob/master/js/graph.js)。
-- `// Set Cartesian coodinate system for the graph (GC)`のブロックで、グラフの座標系を、Canvasの座標系に対して原点 (ctx.originX, ctx.originY)、単位ベクトル (ctx.unitX, ctx.unitY) で定める。ここでは、原点 (300, 500)、単位ベクトル (50, -50) としている。
+- `<script src="https://sekika.github.io/js/graph.js"></script>` では、[graph.js](https://sekika.github.io/js/graph.js) のURLを指定する。このサーバー上の graph.js が書き換えられる可能性があるため、コピーして使うことを推奨する。
+- `var ctx = c.getContext('2d');` で指定されたキャンバスのコンテキスト ctx に対してグラフを描画する。グラフを描画するためパラメータは ctx のプロパティとして設定する。
+- `// Set Cartesian coodinate system for the graph (GC)`のブロックで、グラフの座標系を、ctx の座標系に対して原点 (ctx.originX, ctx.originY)、単位ベクトル (ctx.unitX, ctx.unitY) で定める。ここでは、原点 (300, 500)、単位ベクトル (50, -50) としている。
 - `// Draw graphs` のブロックで、以下のようにグラフを描画している。
-- `drawAxis(ctx);` で座標軸を描画する。
+- `drawAxis(ctx);` で ctx のキャンバスに座標軸を描画する。
+- `drawScaleX(ctx);` でx軸の目盛りを、`drawScaleY(ctx);` でy軸の目盛りを描画する。目盛りの間隔を変えるときには、ctx.scaleX と ctx.scaleY にその値を入れておく。
 - `draw(ctx, exp);` で ctx のキャンバスに exp 関数を描画する。exp 関数はこの JavaScript 内の `function exp(x)` で定義されている。
-- `draw(ctx, plus, 1);` で ctx のキャンバスに plus 関数を描画する。3つ目の引数である1は、plus 関数の2つ目の引数である。このように、draw 関数の3つ目の引数があるときは、draw 関数の2つ目の引数で指定されている関数の2つ目の引数として渡される。関数に複数の引数があるときには、配列変数として渡すことができる。
+- `draw(ctx, plus, 1);` で ctx のキャンバスに plus 関数を描画する。3つ目の引数である1は、plus 関数の2つ目の引数である。このように、draw 関数の3つ目の引数があるときは、draw 関数の2つ目の引数で指定されている関数の2つ目の引数として渡される。関数に複数の引数があるときには、1つの配列変数にまとめれば良い。
 - このように、JavaScript で自由に関数を定義して、その関数を設定した座標系で表示することができる。
 - `ctx.fillText("y = exp(x)", 420,80);` では、[HTML canvas strokeText() Method](https://www.w3schools.com/Tags/canvas_stroketext.asp) で文字を直接描画している。
+- パラメータの一覧は [graph.js](https://github.com/sekika/sekika.github.io/blob/master/js/graph.js) の頭の const で宣言されている定数を参照。たとえば、`const labelX = "x"; // Label of the x axis` はx軸のラベルにはxという文字が設定されていることを意味する。ctx の同じ名前のプロパティ、すなわち ctx.labelX を設定することで、このデフォルトとは異なる値を設定できる。
 
 ## このプログラムの使用例
 
--- js/graph.js
+++ js/graph.js
@@ -9,55 +9,82 @@
 
 'use strict';
 
+// Set default values as constant.
+// These can be changed by setting properties of ctx
+const labelX = "x"; // Label of the x axis
+const labelY = "y"; // Label of the y axis
+const topMargin = 30; // Top margin (without label)
+const rightMargin = 10; // Right margin (without label)
+const vectorWidth = 10; // Width of the vector of axis
+const vectorHeight = 15; // Height of the vector of axis
+
+const scaleX = 1; // Scale interval of x axis
+const scaleY = 1; // Scale interval of y axis
+const scaleLengthX = 6; // Length of scale of x axis
+const scaleLengthY = 5; // Length of scale of y axis
+const offsetScaleY = 30; // Location of label of y axis
+const precision = 10000000; // To prevent rounding error
+
+const plotMinX = 0; // Minimum value of x for plotInt function
+const plotSize = 4; // Size of the mark for the plotInt function
+
 // Draw axis
-function drawAxis(ctx, x, y) {
-    x = x || "x"; // Set default value
-    y = y || "y";
-    // X axis
+function drawAxis(ctx) {
+    // Set default values
+    ctx.labelX = ctx.labelX || labelX;
+    ctx.labelY = ctx.labelY || labelY;
+    ctx.topMargin = ctx.topMargin || topMargin;
+    ctx.rightMargin = ctx.rightMargin || rightMargin; 
+    ctx.vectorWidth = ctx.vectorWidth || vectorWidth;
+    ctx.vectorHeight = ctx.vectorHeight || vectorHeight;
+    // Draw X axis
     ctx.beginPath();
     ctx.moveTo(0, ctx.originY);
-    ctx.lineTo(ctx.width - 30, ctx.originY);
-    ctx.lineTo(ctx.width - 45, ctx.originY - 10);
-    ctx.moveTo(ctx.width - 30, ctx.originY);
-    ctx.lineTo(ctx.width - 45, ctx.originY + 10);
-    ctx.fillText(x, ctx.width - 25, ctx.originY + 5);
-
-    // Y axis
+    ctx.lineTo(ctx.width - ctx.rightMargin, ctx.originY);
+    ctx.lineTo(ctx.width - ctx.rightMargin - ctx.vectorHeight, ctx.originY - ctx.vectorWidth);
+    ctx.moveTo(ctx.width - ctx.rightMargin, ctx.originY);
+    ctx.lineTo(ctx.width - ctx.rightMargin - ctx.vectorHeight, ctx.originY + ctx.vectorWidth);
+    ctx.fillText(ctx.labelX, ctx.width - ctx.rightMargin + 10, ctx.originY + 5);
+    // Draw Y axis
     ctx.moveTo(ctx.originX, ctx.height);
-    ctx.lineTo(ctx.originX, 30);
-    ctx.lineTo(ctx.originX - 10, 45);
-    ctx.moveTo(ctx.originX, 30);
-    ctx.lineTo(ctx.originX + 10, 45);
-    ctx.fillText(y, ctx.originX - 5, 20);
+    ctx.lineTo(ctx.originX, ctx.topMargin);
+    ctx.lineTo(ctx.originX - ctx.vectorWidth, ctx.topMargin + ctx.vectorHeight);
+    ctx.moveTo(ctx.originX, ctx.topMargin);
+    ctx.lineTo(ctx.originX + ctx.vectorWidth, ctx.topMargin + ctx.vectorHeight);
+    ctx.fillText(ctx.labelY, ctx.originX - 5, ctx.topMargin - 10);
     ctx.stroke();
 }
 
 // Draw scale of the x axis
-function drawScaleX(ctx, scaleX) {
-    var x, minX, precision;
-    precision = 10000000;
-    minX = -Math.floor((ctx.originX - 20) / ctx.unitX / scaleX) * scaleX;
-    for (x = minX; ctx.originX + ctx.unitX * x < ctx.width - 30; x = x + scaleX) {
-        x = Math.round(x * precision) / precision;
+function drawScaleX(ctx) {
+    var x, minX;
+    ctx.scaleX = ctx.scaleX || scaleX;
+    ctx.scaleLengthX = ctx.scaleLengthX || scaleLengthX; 
+    ctx.rightMargin = ctx.rightMargin || rightMargin;
+    ctx.precision = ctx.precision || precision;
+    minX = -Math.floor((ctx.originX - 20) / ctx.unitX / ctx.scaleX) * ctx.scaleX;
+    for (x = minX; ctx.originX + ctx.unitX * x < ctx.width - ctx.rightMargin; x = x + ctx.scaleX) {
+        x = Math.round(x * ctx.precision) / ctx.precision;
         ctx.moveTo(ctx.originX + ctx.unitX * x, ctx.originY);
-        ctx.lineTo(ctx.originX + ctx.unitX * x, ctx.originY + 7);
-        ctx.fillStyle = "black";
+        ctx.lineTo(ctx.originX + ctx.unitX * x, ctx.originY + ctx.scaleLengthX);
         ctx.fillText(x, ctx.originX + ctx.unitX * x - 20, ctx.originY + 25);
     }
     ctx.stroke();
 }
 
 // Draw scale of the y axis
-function drawScaleY(ctx, scaleY) {
-    var y, minY, precision;
-    precision = 10000000;
-    ctx.offsetScaleY = ctx.offsetScaleY || 30;
-    minY = Math.ceil((ctx.height - ctx.originY - 10) / ctx.unitY / scaleY) * scaleY;
-    for (y = minY; ctx.originY + ctx.unitY * y > 30; y = y + scaleY) {
-        y = Math.round(y * precision) / precision;
+function drawScaleY(ctx) {
+    var y, minY;
+    ctx.scaleY = ctx.scaleY || scaleY;
+    ctx.scaleLengthY = ctx.scaleLengthY || scaleLengthY;
+    ctx.offsetScaleY = ctx.offsetScaleY || offsetScaleY;
+    ctx.precision = ctx.precision || precision;
+    minY = Math.ceil((ctx.height - ctx.originY - 10) / ctx.unitY / ctx.scaleY) * ctx.scaleY;
+    for (y = minY; ctx.originY + ctx.unitY * y > ctx.topMargin; y = y + ctx.scaleY) {
+        y = Math.round(y * ctx.precision) / ctx.precision;
         if (y !== 0) {
             ctx.moveTo(ctx.originX, ctx.originY + ctx.unitY * y);
-            ctx.lineTo(ctx.originX - 5, ctx.originY + ctx.unitY * y);
+            ctx.lineTo(ctx.originX - ctx.scaleLengthY, ctx.originY + ctx.unitY * y);
             ctx.fillText(y.toString(), ctx.originX - ctx.offsetScaleY, ctx.originY + ctx.unitY * y + 10);
         }
     }
@@ -67,13 +94,15 @@ function drawScaleY(ctx, scaleY) {
 // Draw a graph
 function draw(ctx, func, parameter) {
     var x, y, pixX, pixY, first;
+    ctx.topMargin = ctx.topMargin || topMargin;
+    ctx.rightMargin = ctx.rightMargin || rightMargin;
     ctx.beginPath();
     first = true;
-    for (pixX = 0; pixX < ctx.width - 30; pixX++) {
+    for (pixX = 0; pixX < ctx.width - ctx.rightMargin; pixX++) {
         x = (pixX - ctx.originX) / ctx.unitX;
         y = func(x, parameter);
         pixY = ctx.originY + ctx.unitY * y;
-        if (pixY >= 35 && pixY <= ctx.height) {
+        if (pixY >= ctx.topMargin + 5 && pixY <= ctx.height) {
             if (first) {
                 ctx.moveTo(pixX, pixY);
                 first = false;
@@ -90,15 +119,18 @@ function draw(ctx, func, parameter) {
 // Plot integer values
 function plotInt(ctx, func, parameter) {
     var x, y, pixX, pixY;
-    ctx.plotMinX = ctx.plotMinX || 0;
+    ctx.plotMinX = ctx.plotMinX || plotMinX;
+    ctx.plotSize = ctx.plotSize || plotSize;
+    ctx.topMargin = ctx.topMargin || topMargin;
+    ctx.rightMargin = ctx.rightMargin || rightMargin;
     ctx.beginPath();
-    for (x = ctx.plotMinX; x * ctx.unitX < ctx.width - ctx.originX - 30; x++) {
+    for (x = ctx.plotMinX; x * ctx.unitX < ctx.width - ctx.originX - ctx.rightMargin; x++) {
         y = func(x, parameter);
         pixX = ctx.originX + ctx.unitX * x;
         pixY = ctx.originY + ctx.unitY * y;
-        if (pixY >= 35 && pixY <= ctx.height) {
+        if (pixY >= ctx.topMargin + 5 && pixY <= ctx.height) {
             ctx.beginPath();
-            ctx.arc(pixX, pixY, 4, 0, Math.PI * 2);
+            ctx.arc(pixX, pixY, ctx.plotSize, 0, Math.PI * 2);
             ctx.fill();
         }
     }
