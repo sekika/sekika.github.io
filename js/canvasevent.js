4fd1a5964d6dbf764cc4deb2510ac04f0f4e9b71
Katsutoshi Seki <sekika@users.noreply.github.com>
2020-01-07 03:11:34 +0900

    Published on https://sekika.github.io/2020/01/06/CanvasEvent/

--- js/canvasevent.js
+++ js/canvasevent.js
@@ -1,14 +1,5 @@
----
-layout: post-js
-title: Canvas のマウスイベント
-tag: javascript
----
-Canvas に発生したマウスとタッチのイベントを表示します。ついでに線を書きます。
+// To be loaded from https://sekika.github.io/2020/01/07/CanvasEvent/
 
-<canvas id="canvas" width="400" height="250"
-  style="border:1px solid #000000; background: #ffffe8; max-width: 100%; height: auto; max-height: 100%">
-</canvas>
-<script>
 'use strict';
 var c = document.getElementById('canvas');
 var ctx = c.getContext('2d');
@@ -20,8 +11,7 @@ function move(pos) {
         ctx.moveTo(lastPos[0], lastPos[1]);
         ctx.lineTo(pos[0], pos[1]);
         ctx.stroke();
-        lastPos[0] = pos[0];
-        lastPos[1] = pos[1];
+        lastPos = pos;
     }
 }
 
@@ -42,6 +32,10 @@ function pos(e) {
     return [x, y];
 }
 
+function strPair(pos) {
+    return ' (' + pos[0] + ', ' + pos[1] + ')';
+}
+
 function showMessage(message) {
     var current = document.getElementById("message").textContent;
     message = current + '\n' + message;
@@ -57,30 +51,28 @@ function showMessageTouchMove(message) {
 }
 
 function init() {
-    var i, t, message;
+    var i, message;
     ctx.strokeStyle = "blue";
     ctx.lineWidth = 1;
+    c.addEventListener('mousedown', function(e) {
+        showMessage('mousedown' + strPair(pos(e)));
+        down(pos(e));
+    });
     c.addEventListener('mouseup', function(e) {
-        showMessage('mouseup' + ' (' + pos(e)[0] + ', ' + pos(e)[1] + ')');
+        showMessage('mouseup' + strPair(pos(e)));
         up(pos(e));
     });
     c.addEventListener('click', function(e) {
-        showMessage('click' + ' (' + pos(e)[0] + ', ' + pos(e)[1] + ')');
-    });
-    c.addEventListener('mousedown', function(e) {
-        showMessage('mousedown' + ' (' + pos(e)[0] + ', ' + pos(e)[1] + ')');
-        down(pos(e));
+        showMessage('click' + strPair(pos(e)));
     });
     c.addEventListener('mousemove', function(e) {
-        showMessageMouseMove('mousemove' + ' (' + pos(e)[0] + ', ' +
-            pos(e)[1] + ')');
+        showMessageMouseMove('mousemove' + strPair(pos(e)));
         move(pos(e));
     });
     c.addEventListener('touchstart', function(e) {
         message = 'touchstart';
         for (i = 0; i < e.changedTouches.length; i++) {
-            t = e.changedTouches[i];
-            message += ' (' + pos(t)[0] + ', ' + pos(t)[1] + ')';
+            message += strPair(pos(e.changedTouches[i]));
         }
         showMessage(message);
         if (e.changedTouches.length == 1) {
@@ -90,16 +82,14 @@ function init() {
     c.addEventListener('touchcancel', function(e) {
         message = 'touchcancel';
         for (i = 0; i < e.changedTouches.length; i++) {
-            t = e.changedTouches[i];
-            message += ' (' + pos(t)[0] + ', ' + pos(t)[1] + ')';
+            message += strPair(pos(e.changedTouches[i]));
         }
         showMessage(message);
     });
     c.addEventListener('touchend', function(e) {
         message = 'touchend';
         for (i = 0; i < e.changedTouches.length; i++) {
-            t = e.changedTouches[i];
-            message += ' (' + pos(t)[0] + ', ' + pos(t)[1] + ')';
+            message += strPair(pos(e.changedTouches[i]));
         }
         showMessage(message);
         if (e.changedTouches.length == 1) {
@@ -112,8 +102,7 @@ function init() {
         }
         message = 'touchmove';
         for (i = 0; i < e.changedTouches.length; i++) {
-            t = e.changedTouches[i];
-            message += ' (' + pos(t)[0] + ', ' + pos(t)[1] + ')';
+            message += strPair(pos(e.changedTouches[i]));
         }
         showMessageTouchMove(message);
         if (e.changedTouches.length == 1) {
@@ -123,27 +112,8 @@ function init() {
 }
 
 function clear() {
-    c.width = c.width;
+    ctx.clearRect(0, 0, c.width, c.height);
     document.getElementById("message").innerHTML = '';
-    showMessageMouseMove("(mousemove");
-    showMessageTouchMove("(touchmove");
+    showMessageMouseMove("(mousemove)");
+    showMessageTouchMove("(touchmove)");
 }
-</script>
-
-<form name="formOption">
-<input id="noSwipe" type="checkbox"><label for="noSwipe">Canvas 内のスワイプを無効にする</label>
-<button onclick="clear()">クリア</button>
-</form>
-
-- <div id="messageMouseMove">(mousemove)</div>
-- <div id="messageTouchMove">(touchmove)</div>
-- mousedown, mouseup, click, touchstart, touchend, touchcancel, touchmove
-<pre style='width: 350px; max-height: 300px;'>
-<div id="message"></div>
-</pre>
-
-## 参考サイト
-
-- [Using Touch Events with the HTML5 Canvas](http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html) (Ben Centra, 2014/12/5)
-- [Touch events](https://developer.mozilla.org/ja/docs/Web/API/Touch_events) (MDN web docs)
-- [【連載】第二回HTML5でCanvasアプリを作る入門編 ~マウスクリックイベントについて~](http://nigohiroki.hatenablog.com/entry/2013/01/11/010617) (nigoblog, 2013/1/11)
