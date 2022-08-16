31ed8b2d63a65f6e7919f36b769f57427f865a72
Katsutoshi Seki <sekika@users.noreply.github.com>
2022-08-16 15:38:41 +0900

    rebuild

--- _drafts/rebuild.sh
+++ _drafts/rebuild.sh
@@ -3,6 +3,9 @@
 # Settings > Developer settings > Personal access tokens > Generate new token.
 echo "Make sure that token is saved and it is not expired. See source." 
 token=`cat ~/.config/github/token`
+user="sekika"
+repo="sekika.github.io"
 # https://docs.github.com/ja/rest/pages#request-a-github-pages-build
-command="https://api.github.com/repos/sekika/sekika.github.io/pages/builds"
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $token" $command
+accept="Accept: application/vnd.github+json"
+command="https://api.github.com/repos/$user/$repo/pages/builds"
+curl -X POST -H "$accept" -H "Authorization: token $token" $command
